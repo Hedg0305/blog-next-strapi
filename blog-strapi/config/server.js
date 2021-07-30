4fc5934dc5e42f2493b04e5009d018d6dcd6fb7b
Henrique <henrique.garcia@leanonsystems.com>
2021-07-30 08:35:29 -0300

    first commit

---
+++ blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f05674e4a27d282659017dc70e89ee4c'),
+    },
+  },
+});
---
+++ blog-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fa006040-9842-4412-8ae4-b6c13fc3071f'
+};
